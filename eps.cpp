1bfabc6875ba68e47e14b513e99e35074b5acd6c
unknown <alsoqingzhou@gmail.com>
2021-04-06 22:45:58 +0800

    add callatz.cpp

---
+++ eps.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+const double eps = 1e-8;
+
+#define Equ(a,b) ((fabs((a)-(b))) < (eps))
+
+int main() {
+    double db1 = 4 * asin(sqrt(2.0) / 2);
+    double db2 = 3 * asin(sqrt(3.0) / 2);
+    if(Equ(db1, db2)) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+
+    return 0;
+}
