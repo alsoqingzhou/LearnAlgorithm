d06d0f121c40fd1b7cae02a3111e4b9cd4ef0af6
unknown <alsoqingzhou@gmail.com>
2021-06-12 17:28:12 +0800

    add DFS function

--- dataStructure/graph.cpp
+++ dataStructure/graph.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 const int maxsize = 10010;
 
 // 图的顺序存储结构体定义——利用邻接矩阵
@@ -26,3 +27,23 @@ typedef struct {
     VNode adjlist[maxsize]; //邻接表数组，为顶点表结点数组，保存顶点表结点
     int n, e; //保存结点数与边数
 }AGraph; //图的邻接表类型定义
+
+//基于邻接表的图的深度有限遍历
+int visit[maxsize]; //该数组用于标记图中的某个结点是否已经访问过
+
+void Visit(int v) {
+    printf("%d", v);
+}
+
+void DFS(AGraph *G, int v) {//第一个参数是某个图的指针，第二个参数是图中的开始结点
+    ArcNode *p; //指向结点各条边的指针
+    visit[v] = 1; //当前结点没有访问过，访问数组标记
+    Visit(v); //访问结点
+    p = G->adjlist[v].firstArc; //p指向顶点结点的第一条边
+    while(p != NULL) {
+        if(visit[p->adjVex] == 0) { //当前p边对应的结点没有被访问
+            DFS(G, p->adjVex); //从该结点开始递归
+        }
+        p = p->nextArc; //当前边对应的结点的相邻结点都已经访问结束，p指向下一条边
+    }
+}
