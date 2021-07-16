4ead55cdbdd3c1619a25e73d1468929185321ccf
unknown <alsoqingzhou@gmail.com>
2021-07-16 20:53:28 +0800

    add Q3.1.4-4

--- exercise/stack.cpp
+++ exercise/stack.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 const int maxsize = 50;
 
+// 单链表结构体定义
+typedef struct LinkList{
+    char data;
+    LinkList *next;
+} LinkList;
+
 //Q3.1.4-3
 bool justify(char a[], int n) {
     char stack[maxsize], top = -1;
@@ -23,4 +29,32 @@ bool justify(char a[], int n) {
 
     if(top == -1) return true;
     else return false;
+}
+
+// Q3.1.4-4
+int IsSymmetry(LinkList *l, int n) { //传入参数为单链表头结点和链表长度
+    char stack[maxsize];
+    int top = -1;
+
+    LinkList *p;
+    p = l->next;
+
+    for(int i = 0; i < n/2; ++i) {
+        stack[++top] = p->data;
+        p = p->next;
+    }
+
+    if(n % 2 == 1) { //表长为奇数，跳过中间结点
+        p = p->next; //此时p指向中心对称的后半部分
+    }
+
+    while(p != NULL) {
+        if(p->data != stack[top]) {
+            return 0;
+        }
+        p = p->next;
+        --top;
+    }
+    //走出循环，说明中心对称
+    return 1;
 }
