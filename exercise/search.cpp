c9490fe432ca54f001b69d4e459e77fd08bbbc57
unknown <alsoqingzhou@gmail.com>
2021-07-20 16:51:15 +0800

    add seqSearch and linkSearch function

--- exercise/search.cpp
+++ exercise/search.cpp
@@ -22,3 +22,45 @@ int biSearch_recursive(SSTable st, int low, int high, int key) {
     }
     return 0; //跳出循环，查找失败
 }
+
+// 线性表顺序检索算法优化：将每次查找到的元素提前一位
+// Q7.2.4
+typedef struct linkList {
+    int data;
+    struct linkList *next;
+} linkList; //顺序表的单链表存储
+
+int seqSearch(SSTable &st, int key) {
+    int i;
+    for(i = 0; i < st.length; ++i) {
+        if(st.a[i] == key) {
+            int temp = st.a[i];
+            st.a[i] = st.a[i-1];
+            st.a[i-1] = temp;
+            return i-1;
+        }
+    }
+    if(i >= st.length) {
+        return 0;
+    }
+}
+
+int linkSearch(linkList* l, int key) {
+    linkList *p, *q, *r;
+    p = l;
+    while(p->next->next != NULL) {
+        if(p->next->next->data == key) {
+            q = p->next;
+            r = q->next->next;
+            q->next = r->next;
+            r->next = q;
+            p->next = r;
+            return 1;
+        }
+    }
+    if(l->next->data == key && l->next->next == NULL) {
+        return 1; //该单链表只有一个元素，就是第一个
+    } else {
+        return 0;
+    }
+}
